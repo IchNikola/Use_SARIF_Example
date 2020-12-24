cbc3139892f30ca75bdd0724299426c8359e020c
IchNikola <55153245+IchNikola@users.noreply.github.com>
2020-12-24 17:31:32 +0300

    Add files via upload

--- BestCode.cpp
+++ BestCode.cpp
@@ -7,5 +7,14 @@ int main()
     a = a;
 } 
 
-void ff(int a, int b) {
+int DoSomething(int a)
+{
+    return a * rand() / 10;
+}
+
+int ff(int a, int b) 
+{
+    a = DoSomething(a);
+    b = DoSomething(a);
+    return a + b;
 }
