2d82b9024bf47b70198891216572b52ebf4b9723
IchNikola <55153245+IchNikola@users.noreply.github.com>
2020-12-24 18:03:51 +0300

    Update BestCode.cpp

--- BestCode.cpp
+++ BestCode.cpp
@@ -7,9 +7,5 @@ int main()
     a = a;
 } 
 
-void ff(float array[3])
-{
-    size_t n = sizeof(array) / sizeof(array[0]);
-    for (size_t i = 0; i != n; i++)
-        array[i] = 1.0f;
+void ff(){
 }
